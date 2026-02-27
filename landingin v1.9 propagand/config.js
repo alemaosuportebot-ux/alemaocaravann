e5f635e45740a72be83cfc6b3f9021c0a3fb4967
alemaosuportebot-ux <alemaosuportebot@gmail.com>
2026-02-26 21:56:16 -0300

    Delete landingin v1.9 propagand directory

--- landingin v1.9 propagand/config.js
+++
@@ -1,4 +0,0 @@
-const CONFIG = {
-    // URL do seu Google Apps Script (Backend)
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyi7gaoaJ3UvawV_M-VJndHxdSI_NKvfKaf0AlqeaPSBCYVoqSJKkIkOOtgbEmKxEgFzw/exec'
-};
